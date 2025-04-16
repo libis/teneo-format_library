3fac44aa764dbf85ae223b0239a14aa9a01e780e
Kris Dekeyser <kris.dekeyser@kuleuven.be>
2025-04-16 12:00:13 +0200

    remove binaries installed from gems

--- db/seeds/20250101_formats.rb
+++ db/seeds/20250101_formats.rb
@@ -3,15 +3,9 @@
 require 'teneo/format_library/format'
 
 Sequel.seed(:production, :development, :test) do
-  # Loads CSV files from the format library directory and updates or creates
-  # format entries in the database.
-  #
-  # For each CSV file found, it extracts the library version from the filename,
-  # reads the file, and processes each row. The row data is merged with the
-  # library version and passed to update_or_create to update or create a format
-  # entry in the Teneo::DataModel::Format model. The CSV header conversion is
-  # done using the header_converter method.
-
+  # Seeds the database with all formats from PRONOM and LOC signature files and
+  # extra formats from the directory specified by the `FORMAT_LIBRARY_SEEDS_FORMAT_DIR`
+  # environment variable (defaulting to `db/seeds/data/formats/`).
   def run
     puts 'Loading PRONOM Signatures data ...'
     Teneo::FormatLibrary::Format.load_pronom_signatures
--- db/seeds/20250102_tags.rb
+++ db/seeds/20250102_tags.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 Sequel.seed(:production, :development, :test) do
+  # Load all YAML files in the seeds directory and create or update the tags in
+  # the database. The seeds directory is determined by the
+  # `FORMAT_LIBRARY_SEEDS_TAG_DIR` environment variable. If this variable is not
+  # set, the default directory is `db/seeds/data/tags`.
   def run
     puts 'Loading Format tags ...'
     seeds_data_dir = ENV.fetch('FORMAT_LIBRARY_SEEDS_TAG_DIR', File.join(File.dirname(__FILE__), 'data', 'tags'))
