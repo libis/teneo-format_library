5a7e929c10a81ed9d4a853e30617d8dac7713810
Kris Dekeyser <kris.dekeyser@kuleuven.be>
2026-06-21 22:39:07 +0200

    removing downloaded dependencies from the repository

--- gem/vendor/bundle/ruby/3.4.0/gems/console-1.35.1/lib/console/version.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Released under the MIT License.
-# Copyright, 2019-2026, by Samuel Williams.
-
-module Console
-	VERSION = "1.35.1"
-end
--- gem/vendor/bundle/ruby/3.4.0/gems/github_changelog_generator-1.18.0/lib/github_changelog_generator/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module GitHubChangelogGenerator
-  VERSION = "1.18.0"
-end
